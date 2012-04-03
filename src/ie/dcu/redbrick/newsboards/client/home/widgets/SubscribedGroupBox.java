88f33a1a93f6d5ad622058e2f4b15eaff7de12fc
Andrew Martin <werdz@redbrick.dcu.ie>
2012-04-04 00:19:17 +0100

    Left out a file

---
+++ src/ie/dcu/redbrick/newsboards/client/home/widgets/SubscribedGroupBox.java
@@ -0,0 +1,7 @@
+package ie.dcu.redbrick.newsboards.client.home.widgets;
+
+import com.google.gwt.event.dom.client.HasClickHandlers;
+
+public interface SubscribedGroupBox {
+    HasClickHandlers getUnsubscribeButton();
+}
